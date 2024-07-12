e0f4590462a521c01f424364ecd1124f69ca9c97
MariamMchedlishvili <mmchedlishvili07@gmail.com>
2024-07-12 18:26:06 +0400

    commit

---
+++ Level 26/homework/main.py
@@ -0,0 +1,282 @@
+for i in range(1, 6):
+    print(i)
+
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+for i in "hello":
+    print(i)
+
+string = "hello"
+i = 0
+while i < len(string):
+    print(string[i])
+    i += 1
+
+for i in range(1, 4):
+    print(i)
+
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+
+for i in range(3):
+    print("Python is fun")
+
+i = 0
+while i < 3:
+    print("Python is fun")
+    i += 1
+
+my_list = [1, 2, 3]
+for element in my_list:
+    print(element)
+
+my_list = [1, 2, 3]
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+
+for i in [1, 2, 3]:
+    print("hi")
+
+my_list = [1, 2, 3]
+i = 0
+while i < len(my_list):
+    print("hi")
+    i += 1
+
+
+for i in range(1, 4):
+    print(i)
+
+
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+
+
+for i in range(3, 0, -1):
+    print(i)
+
+
+i = 3
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+for i in ['a', 'b', 'c', 'd']:
+    print(i)
+
+
+alphabet = ['a', 'b', 'c', 'd']
+i = 0
+while i < len(alphabet):
+    print(alphabet[i])
+    i += 1
+
+
+for _ in range(4):
+    print("looping")
+
+
+i = 0
+while i < 4:
+    print("looping")
+    i += 1
+
+
+
+tuple = (1, 2, 3)
+for element in tuple:
+    print(element)
+
+tuple = (1, 2, 3)
+i = 0
+while i < len(tuple):
+    print(tuple[i])
+    i += 1
+
+
+
+tuple = (1, 2, 3)
+i = 0
+while i < len(tuple):
+    print(tuple[i])
+    i += 1
+
+
+for i in range(5, 0, -1):
+    print(i)
+
+i = 5
+while i > 0:
+    print(i)
+    i -= 1
+
+
+list = ["apple", "banana", "cherry"]
+for fruit in list:
+    print(fruit)
+
+list = ["apple", "banana", "cherry"]
+i = 0
+while i < len(list):
+    print(list[i])
+    i += 1
+
+
+for i in range(3):
+    print(i)
+
+
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+
+
+
+for i in [1, 2, 3, 4]:
+    print("a,b,c")
+
+
+list = [1, 2, 3, 4]
+i = 0
+while i < len(list):
+    print("a,b,c")
+    i += 1
+
+
+
+for i in "abc":
+    print(i)
+
+
+string = "abc"
+i = 0
+while i < len(string):
+    print(string[i])
+    i += 1
+
+
+list = ["x", "y", "z"]
+for i in range(2):
+    print(list[i])
+
+
+list = ["x", "y", "z"]
+i = 0
+while i < 2:
+    print(list[i])
+    i += 1
+
+for i in range(2):
+    print("Hello World")
+ 
+
+
+i = 0
+while i < 2:
+    print("Hello World")
+    i += 1
+
+
+list = {1, 2, 3}
+for element in list:
+    print(element)
+
+
+
+list= {"a": 1, "b": 2}
+for a, b  in list.items():
+    print(f"{a}: {b}")
+
+
+
+for number in [10, 20, 30]:
+    print(number)
+
+
+numbers = [10, 20, 30]
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
+
+
+for i in range(3):
+    pass
+print("Done")
+
+
+i = 0
+while i < 3:
+    i += 1
+print("Done")
+
+
+
+list = [[1, 2], [3, 4]]
+for i in list:
+    for element in i:
+        print(element)
+
+
+list = [[1, 2], [3, 4]]
+i = 0
+while i < len(list):
+    num = 0
+    while num < len(list[i]):
+        print(list[i][num])
+        num += 1
+    i += 1
+
+for i in range(1, 6):
+    print(i)
+
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+
+
+for i in "loop":
+    print(i)
+
+
+
+string = "loop"
+i = 0
+while i < len(string):
+    print(string[i])
+    i += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
